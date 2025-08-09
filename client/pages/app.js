755b65e08ab688423f2ded501dc78adf8450c1c1
Mathew Oreoluwa Solomon <100151674+Solomonmatt@users.noreply.github.com>
2025-08-09 12:44:03 +0100

    Add files via upload

---
+++ client/pages/app.js
@@ -0,0 +1,14 @@
+import { appWithTranslation } from 'next-i18next';
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default appWithTranslation(MyApp);
